5beecf59f4170a4d4ae9a13586f5ccfd3a5c789e
Renald William <renald_w@hotmail.com>
2020-07-16 22:10:19 -0300

    Classe Campo: Atributos

--- src/br/com/snow/cm/modelo/Campo.java
+++ src/br/com/snow/cm/modelo/Campo.java
@@ -1,5 +1,25 @@
 package br.com.snow.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+	
+	private boolean minado = false;
+	private boolean aberto = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinho = new ArrayList<>();
+	
+	Campo(int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		return true;
+	}
 }
--- src/br/com/snow/cm/modelo/Tabuleiro.java
+++ src/br/com/snow/cm/modelo/Tabuleiro.java
@@ -1,5 +1,6 @@
 package br.com.snow.cm.modelo;
 
 public class Tabuleiro {
-
+	
+	
 }
