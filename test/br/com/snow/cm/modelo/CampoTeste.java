49615f2bcdb3abb1a655991f18d850a24fac0faa
Renald William <renald_w@hotmail.com>
2020-07-16 22:58:09 -0300

    Logica Campo #01

---
+++ test/br/com/snow/cm/modelo/CampoTeste.java
@@ -0,0 +1,62 @@
+package br.com.snow.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
+
+	@Test
+	void testeVizinhoDistancia1Esquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Direita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Emcima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1EmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia2() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNãoVizinhoDistancia2() {
+		Campo vizinho = new Campo(1, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+
+	
+	
+}
