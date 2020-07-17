454bee82941e47c7cb949999709b1ae62a23b633
Renald William <renald_w@hotmail.com>
2020-07-17 20:40:33 -0300

    Logica Tabuleiro #015

--- src/br/com/snow/cm/modelo/Tabuleiro.java
+++ src/br/com/snow/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.snow.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -26,7 +27,7 @@ public class Tabuleiro {
 
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
-			for (int coluna = 0; coluna < colunas ; coluna++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
 				campos.add(new Campo(linha, coluna));
 			}
 		}
@@ -34,14 +35,34 @@ public class Tabuleiro {
 
 	private void associarVizinhos() {
 		for (Campo c1 : campos) {
-			for (Campo c2: campos) {
+			for (Campo c2 : campos) {
 				c1.adicionarVizinho(c2);
-			} 
+			}
 		}
 	}
 
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
 
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+
+		} while (minasArmadas < minas);
 	}
 
+	public boolean objetivosAlcancados() {
+		return campos.stream().allMatch(c -> c.objetivosAlcancados());
+	}
+
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+		return " ";
+	}
 }
